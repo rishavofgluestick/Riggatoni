e33c6c009f6056052779f20bc077669b2de3f146
rishavsingh <rishav4xyz@gmail.com>
2023-12-10 21:03:57 -0800

    Added round 2 and fixed win and lose conditions

--- src/GameDisplay.java
+++ src/GameDisplay.java
@@ -10,24 +10,39 @@ public class GameDisplay extends PApplet {
     private ArrayList<Bullet> bullets = new ArrayList<>();
     private int width=800;
     private int height =800;
+    private int round=1;
 
     public void settings() {
         size(width, height);//Adjust the screen
     }
 
     public void setup() {
-        mario = new Mario(25,370,60,60,1);
-        for (int i = 150; i <= 300; i+=100) {
-            carts.add(new Cart(25, i+20, 60, 60, 1,0));
+        if(round==1){
+            mario = new Mario(25,370,60,60,1);
+            for (int i = 150; i <= 300; i+=100) {
+                carts.add(new Cart(25, i+20, 60, 60, 1,0));
+            }
+            for (int i = 450; i <= 550; i+=100) {
+                carts.add(new Cart(25, i+20, 60, 60, 1,0));
+            }
+            int sum = 0;
+            for (int i = 0; i < carts.size(); i++) {
+                sum += carts.get(i).getY();
+            }
         }
-        for (int i = 450; i <= 550; i+=100) {
-            carts.add(new Cart(25, i+20, 60, 60, 1,0));
+        if(round==2){
+            mario = new Mario(25,370,60,60,1);
+            for (int i = 150; i <= 300; i+=100) {
+                carts.add(new Cart(100, i+20, 60, 60, 1,0));
+            }
+            for (int i = 450; i <= 550; i+=100) {
+                carts.add(new Cart(100, i+20, 60, 60, 1,0));
+            }
+            int sum = 0;
+            for (int i = 0; i < carts.size(); i++) {
+                sum += carts.get(i).getY();
+            }
         }
-        int sum = 0;
-        for (int i = 0; i < carts.size(); i++) {
-            sum += carts.get(i).getY();
-        }
-        int middleY = sum / carts.size();//Setup Mario in the middle
 
     }
 
@@ -69,17 +84,46 @@ public class GameDisplay extends PApplet {
 
     }
     public void checkEnd() {
-        if (mario.getX() > height - 50) { // assuming the end is at the bottom of the screen
-            println("You win");
+        textSize(43);
+        if (mario.getX() > width - 60 && round==2) { // assuming the end is at the bottom of the screen
+            background(0);
+            fill(0,255,0);
+            textSize(100);
+            text("You WIN!", 180,380);
+
             noLoop(); // stop the game
         }
+        if (mario.getX() > width - 60 && round==1) { // assuming the end is at the bottom of the screen
+            for (int i = carts.size()-1; i >=0; i--) {
+                carts.remove(i);
+            }
+            mario = new Mario(25,370,60,60,1);
+            for (int i = 150; i <= 300; i+=100) {
+                carts.add(new Cart(100, i+20, 60, 60, 2,0));
+            }
+            for (int i = 450; i <= 550; i+=100) {
+                carts.add(new Cart(100, i+20, 60, 60, 2,0));
+            }
+            int sum = 0;
+            for (int i = 0; i < carts.size(); i++) {
+                sum += carts.get(i).getY();
+            }
+            round++;
+
+        }
         for (Cart cart : carts) {
-            if (mario.getBounds().intersects(cart.getBounds())) {
-                println("Fail");
+            if (cart.getX()>width-60) {
+                background(0);
+                fill(255,0,0);
+                textSize(100);
+                text("You Lose!", 170, 380);
                 noLoop(); // stop the game
             }
         }
-        System.out.println();
+
+    }
+    public void text(){
+        checkEnd();
     }
     public void fullScreen(){
         fullScreen();
